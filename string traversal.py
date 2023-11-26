bfe61630c4d29257cc74f0ef886e8b312539f80c
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-26 21:30:16 +0545

    stringtraversal_in_python

---
+++ string traversal.py
@@ -0,0 +1,17 @@
+ # treversing without index using for loop
+
+a='apekshya'
+for c in a:
+    print(c) 
+
+#with index  
+
+n=len(a)
+for i in range(n):
+    print(a[i])
+
+# while
+i=0
+while i<n:
+    print(a[i])
+    i+=1
